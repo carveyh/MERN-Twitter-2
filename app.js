75a80ab137d233902a48fb55774f24c8f0f01ad0
carveyh <carvey.hor@gmail.com>
2023-06-11 06:35:30 -0400

    Login functionality add

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
 require('./models/User');
+require('./config/passport');
+const passport = require('passport');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
+
+app.use(passport.initialize());
 
 app.use(logger('dev'));
 app.use(express.json());
