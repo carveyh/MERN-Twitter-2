bb8c25923f9420e211fd9fd6eccdfa965b7cfcd3
carveyh <carvey.hor@gmail.com>
2023-06-11 06:03:42 -0400

    Error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const debug = require('debug');
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -33,8 +35,31 @@ app.use(
 	})
 )
 
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
